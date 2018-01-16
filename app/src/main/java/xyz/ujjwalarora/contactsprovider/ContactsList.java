8d63ccecd399e889c229f649c43d5cdb86f4755b
Ujjwal Arora <ujjwal@Ujjwals-MacBook-Pro.local>
2018-01-16 12:19:24 +0530

    Add ListView and Fragment for Contacts

---
+++ app/src/main/java/xyz/ujjwalarora/contactsprovider/ContactsList.java
@@ -0,0 +1,47 @@
+package xyz.ujjwalarora.contactsprovider;
+
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.provider.ContactsContract;
+import android.widget.AdapterView;
+
+
+public class ContactsList extends Fragment implements
+        LoaderManager.LoaderCallbacks<Cursor>,
+        AdapterView.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contacts_list, container, false);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
